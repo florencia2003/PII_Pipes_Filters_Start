4ead71e30a9a1a89976867134da390b237a82300
florencia2003 <maria.correaj@correo.ucu.edu.uy>
2022-10-24 08:41:35 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,20 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture("beer.jpg");
+
+            PipeNull pipeNull = new PipeNull();
+
+            FilterNegative filter2 = new FilterNegative();
+            FilterGreyscale filter1 = new FilterGreyscale();
+
+            PipeSerial piperSerial2 = new PipeSerial(filter2, pipeNull);
+            PipeSerial piperSerial1 = new PipeSerial(filter1, piperSerial2);
+
+
+            IPicture image = piperSerial1.Send(picture);
+            pipeNull.Send(image);
         }
     }
 }
